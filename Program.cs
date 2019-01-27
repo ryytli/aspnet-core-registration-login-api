459831ea8edb0b3f8cd568cbf96fa53b9f51d8ed
Lauri Rüütli <lauri@soft.ee>
2019-01-27 20:54:11 +0200

    port muudetud 4000

--- Program.cs
+++ Program.cs
@@ -14,7 +14,7 @@ namespace WebApi
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
-                .UseUrls("http://localhost:8580")
+                .UseUrls("http://localhost:4000")
                 .Build();
     }
 }
